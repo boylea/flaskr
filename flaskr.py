ea2d268b765b2dbdb31812e8b57b822d31a8a18f
Amy Boyle <aboyle@newrelic.com>
2015-06-30 20:18:08 -0700

    database not cleared if existing

--- flaskr.py
+++ flaskr.py
@@ -1,3 +1,5 @@
+import os
+
 import sqlite3
 from contextlib import closing
 from flask import Flask, request, session, g, redirect, url_for, \
@@ -7,7 +9,7 @@ import newrelic.agent
 newrelic.agent.initialize('newrelic.ini')
 
 # configuration
-DATABASE = '/tmp/flaskr.db'
+DATABASE = 'db/flaskr.db'
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
@@ -22,10 +24,12 @@ def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
 def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+    if not os.path.isfile(app.config['DATABASE']):
+        print 'creating DB'
+        with closing(connect_db()) as db:
+            with app.open_resource('schema.sql', mode='r') as f:
+                db.cursor().executescript(f.read())
+            db.commit()
 
 @app.before_request
 def before_request():
